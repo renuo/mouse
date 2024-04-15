58415a0387a123693802f35d1af1d48a08bfe802
Pascal Andermatt <p.andermatt@me.com>
2024-04-15 16:09:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ Usage instructions:
 - If you are installing: `python setup.py install`
 - If you are developing: `python setup.py sdist --format=zip bdist_wheel --universal bdist_wininst && twine check dist/*`
 """
-import mouse
 
 from setuptools import setup
 setup(
